5b3df9cca90ed487c956fd0805d95734c6c7b44f
Wander Hillen <wjw.hillen@gmail.com>
2019-09-01 15:20:03 +0200

    Initial commit, add some shards

---
+++ src/activerecord-test.cr
@@ -0,0 +1,13 @@
+require "json"
+require "jennifer"
+require "jennifer_sqlite3_adapter"
+
+Jennifer::Config.configure do |conf|
+  conf.adapter = "sqlite3"
+  conf.host = "."
+  conf.db = "test.db"
+end
+
+module Activerecord::Test  
+  puts "biep!"
+end
